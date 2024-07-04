ef3a4994597430497174657990d66313f94c7c70
lexandfr <alexfloresrayme@gmail.com>
2024-07-04 09:12:03 -0500

    form

---
+++ form.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+
+    // Captura los datos del formulario
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const telefono = document.getElementById("telefono").value;
+    console.log(nombre, email, telefono);
+
+    // Almacena los datos en el almacenamiento local del navegador
+    localStorage.setItem("nombre", nombre);
+    localStorage.setItem("email", email);
+    localStorage.setItem("telefono", telefono);
+
+    // Redirige a la página de resultado
+    window.location.href = "resultado.html";
+  });
